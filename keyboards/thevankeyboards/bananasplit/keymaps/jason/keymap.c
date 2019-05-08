c648d48c46dc14e28758ca5aa4a5453ec6191a1c
Jason Woan <jaywoan@gmail.com>
2019-05-07 22:13:48 -0700

    Add bananasplit keymap

--- keyboards/thevankeyboards/bananasplit/keymaps/jason/keymap.c
+++ keyboards/thevankeyboards/bananasplit/keymaps/jason/keymap.c
@@ -1,10 +1,10 @@
-#include "bananasplit.h"
+#include QMK_KEYBOARD_H
 
 #define DEFAULT_LAYER 0
 #define LAYER_1       1
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [DEFAULT_LAYER] = KEYMAP_HHKB_ARROW(
+  [DEFAULT_LAYER] = LAYOUT_hhkb_arrow(
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV, \
     KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSPC, \
     KC_LCTL,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,   KC_QUOT, KC_ENT, \
@@ -12,14 +12,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL,  KC_LGUI,  KC_LALT, KC_LGUI,      KC_ENT,      KC_SPC,  KC_RALT, KC_LEFT,  KC_DOWN, KC_UP, KC_RIGHT
   ),
 
-  [LAYER_1] = KEYMAP_HHKB_ARROW( \
+  [LAYER_1] = LAYOUT_hhkb_arrow( \
     _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,_______,_______, \
     _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, KC_UP,_______,_______, \
     _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_LEFT,KC_RGHT,_______, \
     _______,RGB_TOG,RGB_MOD,RGB_HUI,RGB_HUD,RGB_SAI,RGB_SAD,RGB_VAI,RGB_VAD,_______,KC_DOWN,_______,_______, \
     _______, _______, _______,          _______, _______, _______,          _______, _______, _______, _______, _______ \
   ),
-};
-
-const uint16_t PROGMEM fn_actions[] = {
 };
